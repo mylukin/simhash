82721973cfb3dcf25978294e875f38e316f28803
Lukin <mylukin@gmail.com>
2020-06-22 17:35:26 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/mylukin/simhash
+
+go 1.14
+
+require (
+	github.com/go-dedup/megophone v0.0.0-20170830025436-f01be21026f5 // indirect
+	github.com/go-dedup/simhash v0.0.0-20170904020510-9ecaca7b509c // indirect
+	github.com/go-dedup/text v0.0.0-20170907015346-8bb1b95e3cb7 // indirect
+)
---
+++ simhash.go
@@ -0,0 +1,43 @@
+package simhash
+
+import (
+	gdSimhash "github.com/go-dedup/simhash"
+	"regexp"
+)
+
+type Simhash struct {
+	gdSimhash.SimhashBase
+}
+
+type WordFeatureSet struct {
+	gdSimhash.WordFeatureSet
+}
+
+func NewSimhash() *Simhash {
+	return &Simhash{}
+}
+
+func (st *Simhash) NewWordFeatureSet(b []byte) *WordFeatureSet {
+	fs := &WordFeatureSet{gdSimhash.WordFeatureSet{b}}
+	fs.Normalize()
+	return fs
+}
+
+var boundaries = regexp.MustCompile(`[\w']+(?:\://[\w\./]+){0,1}`)
+
+func (w *WordFeatureSet) GetFeatures() []gdSimhash.Feature {
+	var features []gdSimhash.Feature
+	//words := string(w.B)
+	//for _, w := range words {
+	//	if len(string(w)) > 1 {
+	//		feature := gdSimhash.NewFeature([]byte(string(w)))
+	//		features = append(features, feature)
+	//	}
+	//}
+	bWords := boundaries.FindAll(w.B, -1)
+	for _, w := range bWords {
+		feature := gdSimhash.NewFeature(w)
+		features = append(features, feature)
+	}
+	return features
+}
